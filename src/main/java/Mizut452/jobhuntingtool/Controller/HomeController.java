4315ed9d6b95636a81e242f06078d982ca46defb
M1zut4 <ssvror444@gmail.com>
2022-11-26 13:21:13 +0900

    Initial commit

---
+++ src/main/java/Mizut452/jobhuntingtool/Controller/HomeController.java
@@ -0,0 +1,7 @@
+package Mizut452.jobhuntingtool.Controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+}
---
+++ src/test/java/Mizut452/jobhuntingtool/JobHuntingToolApplicationTests.java
@@ -0,0 +1,13 @@
+package Mizut452.jobhuntingtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobHuntingToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
